77ce41bccfd3273ea80463d785bcf2b0221ccfdb
Anastasia27042001 <obukhova.nastya2017@yandex.ru>
2025-06-09 15:52:41 +0300

    создание апи и ручек по добавлению и поиску пользователей

--- main.py
+++ main.py
@@ -1,19 +1,21 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
-from typing import list, Optional
+from typing import List, Optional
+from validator_data import validate_user_data
 app = FastAPI()
 
 class User(BaseModel):
-    name: str,
-    last_name: str,
-    age: int,
-    phone_number: int,
-    address: str,
-    email: Optional[str] = None
+    first_name: str
+    last_name: str
+    age: int
+    phone_number: str
+    city: str
+    address: str
+    email: str
 
 user_data = []
 
-@app.get('/users', response_model=list[User])
+@app.get('/users', response_model=List[User])
 def get_users():
     return user_data
 
@@ -23,8 +25,8 @@ def add_user(user: User):
     return {'status': 'Пользователь добавлен', 'data': user}
 
 app.get('/users/name/{name}')
-def find_by_name(name: str):
-    result = [i for i in user_data if i['name'].lower() == name.lower()]
+def find_by_name(first_name: str):
+    result = [i for i in user_data if i['first_name'].lower() == first_name.lower()]
     return {'status': 'Пользователь найден', 'data': result} if result else {'status': 'Нет совпадений'}
 
 app.get('/users/last_name/{last_name}')
@@ -37,7 +39,12 @@ def find_by_phone_number(phone_number: int):
     result = [i for i in user_data if i['phone_number'] == phone_number]
     return {'status': 'Пользователь найден', 'data': result} if result else {'status': 'Пользователь не найден'}
 
+app.get('/users/city/{city}')
+def find_by_city(city: str):
+    result = [i for i in user_data if i['city'].lower() == city.lower()]
+    return {'status': 'Пользователи найдены', 'data': result} if result else {'status': 'Пользователи не найден'}
+
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=80)
+    uvicorn.run(app, host="127.0.0.1", port=8000)
 
