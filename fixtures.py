0754c3c8e8624de9cd3f9dec04a977cbb12ad0d9
Anastasia27042001 <obukhova.nastya2017@yandex.ru>
2025-06-10 17:49:40 +0300

    добавление фикстуры с удалением данных перед тестами

--- fixtures.py
+++ fixtures.py
@@ -1,18 +1,24 @@
 import pytest
 from faker import Faker
 import random
+from main import user_data
 
 base_url = 'http://localhost:8000/users'
 fake = Faker('ru')
+
+@pytest.fixture(autouse=True, scope="function")
+def clear_user_data():
+    user_data.clear()
+    yield
+    user_data.clear()
 
 @pytest.fixture(scope='session')
 def create_valid_user():
     return {
         'first_name': fake.first_name(),
         'last_name': fake.last_name(),
-        'age': fake.random_int(min=18, max=100),
+        'age': fake.random_int(min=18, max=99),
         'phone_number': fake.phone_number(),
-        'city': fake.city(),
         'address': fake.address(),
         'email': fake.email()
     }
@@ -27,7 +33,7 @@ def create_invalid_user(create_valid_user):
     invalid_cases.append(case1)
 
     case2 = valid.copy()
-    case2['age'] = fake.word()
+    case2['age'] = fake.random_int(min=-100, max=0),
     invalid_cases.append(case2)
 
 
@@ -36,16 +42,12 @@ def create_invalid_user(create_valid_user):
     invalid_cases.append(case3)
 
     case4 = valid.copy()
-    case4['phone_number'] = 1234567890
+    case4['phone_number'] = "234"
     invalid_cases.append(case4)
 
     case5 = valid.copy()
-    case5['city'] = ''
+    case5['address'] = ''
     invalid_cases.append(case5)
-
-    case6 = valid.copy()
-    case6['address'] = None
-    invalid_cases.append(case6)
 
     return random.choice(invalid_cases)
 
